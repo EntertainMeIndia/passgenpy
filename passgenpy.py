845ca9e66aeb06b14387ae91f69e7f8ac275804b
EntertainMeIndia <124145044+EntertainMeIndia@users.noreply.github.com>
2023-05-02 09:56:14 +0530

    Update passgenpy.py

--- passgenpy.py
+++ passgenpy.py
@@ -1,43 +1,48 @@
 import random
-import tkinter as tk
 import pyperclip
 
 class PasswordGeneratorApp:
-    def __init__(self, master):
-        self.master = master
-        master.title("PyPassword Generator")
+    def __init__(self):
+        self.root = document.getElementById('root')
+        
+        self.letters_label = document.createElement('label')
+        self.letters_label.textContent = "How many letters would you like in your password?"
+        self.root.appendChild(self.letters_label)
 
-        self.letters_label = tk.Label(master, text="How many letters would you like in your password?")
-        self.letters_label.pack()
+        self.letters_entry = document.createElement('input')
+        self.root.appendChild(self.letters_entry)
 
-        self.letters_entry = tk.Entry(master)
-        self.letters_entry.pack()
+        self.symbols_label = document.createElement('label')
+        self.symbols_label.textContent = "How many symbols would you like?"
+        self.root.appendChild(self.symbols_label)
 
-        self.symbols_label = tk.Label(master, text="How many symbols would you like?")
-        self.symbols_label.pack()
+        self.symbols_entry = document.createElement('input')
+        self.root.appendChild(self.symbols_entry)
 
-        self.symbols_entry = tk.Entry(master)
-        self.symbols_entry.pack()
+        self.numbers_label = document.createElement('label')
+        self.numbers_label.textContent = "How many numbers would you like?"
+        self.root.appendChild(self.numbers_label)
 
-        self.numbers_label = tk.Label(master, text="How many numbers would you like?")
-        self.numbers_label.pack()
+        self.numbers_entry = document.createElement('input')
+        self.root.appendChild(self.numbers_entry)
 
-        self.numbers_entry = tk.Entry(master)
-        self.numbers_entry.pack()
+        self.generate_button = document.createElement('button')
+        self.generate_button.textContent = "Generate Password"
+        self.generate_button.addEventListener('click', self.generate_password)
+        self.root.appendChild(self.generate_button)
 
-        self.generate_button = tk.Button(master, text="Generate Password", command=self.generate_password)
-        self.generate_button.pack()
+        self.password_label = document.createElement('label')
+        self.root.appendChild(self.password_label)
 
-        self.password_label = tk.Label(master, text="")
-        self.password_label.pack()
-
-        self.copy_button = tk.Button(master, text="Copy Password", command=self.copy_password)
-        self.copy_button.pack()
+        self.copy_button = document.createElement('button')
+        self.copy_button.textContent = "Copy Password"
+        self.copy_button.addEventListener('click', self.copy_password)
+        self.root.appendChild(self.copy_button)
 
     def generate_password(self):
-        nr_letters = int(self.letters_entry.get())
-        nr_symbols = int(self.symbols_entry.get())
-        nr_numbers = int(self.numbers_entry.get())
+        nr_letters = int(self.letters_entry.value)
+        nr_symbols = int(self.symbols_entry.value)
+        nr_numbers = int(self.numbers_entry.value)
 
         letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
         numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -63,12 +68,10 @@ class PasswordGeneratorApp:
         for char in password_list:
             password += char
 
-        self.password_label.config(text=f"Your password is: {password}")
+        self.password_label.textContent = f"Your password is: {password}"
         self.generated_password = password
 
     def copy_password(self):
         pyperclip.copy(self.generated_password)
 
-root = tk.Tk()
-app = PasswordGeneratorApp(root)
-root.mainloop()
+app = PasswordGeneratorApp()
